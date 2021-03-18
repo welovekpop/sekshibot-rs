84222594edf764b01ed724e31e092e9d4d556c9d
Renée Kooi <renee@kooi.me>
2021-03-18 16:30:23 +0100

    deps: use surf for Neocities API

--- src/api/neocities.rs
+++ src/api/neocities.rs
@@ -1,8 +1,8 @@
-use hreq::prelude::*;
-use hreq::Agent;
+use crate::IntoAnyhow;
 use serde::Deserialize;
 use sha1::Sha1;
 use std::io::Cursor;
+use surf::Request;
 use thiserror::Error;
 use yolofd::FormData;
 
@@ -10,10 +10,8 @@ use yolofd::FormData;
 pub enum PublishError {
     #[error("missing neocities username/password")]
     MissingAuth,
-    #[error(transparent)]
-    HttpError(#[from] hreq::http::Error),
-    #[error(transparent)]
-    HreqError(#[from] hreq::Error),
+    #[error("http error")]
+    SurfError(#[from] anyhow::Error),
     #[error("{0}")]
     NeocitiesError(String),
     #[error(transparent)]
@@ -35,13 +33,12 @@ struct ListResponse {
     result: String,
 }
 
-fn list(username: &str, password: &str) -> Request<hreq::Body> {
+fn list(username: &str, password: &str) -> Request {
     let auth = base64::encode(format!("{}:{}", username, password));
 
-    Request::get("https://neocities.org/api/list")
+    surf::get("https://neocities.org/api/list")
         .header("authorization", format!("Basic {}", auth))
-        .with_body(())
-        .unwrap()
+        .build()
 }
 
 /// Returns the URL to the page.
@@ -49,10 +46,13 @@ pub async fn publish(page_name: &str, content: &str) -> Result<String, PublishEr
     let username = std::env::var("NEOCITIES_USERNAME").map_err(|_| PublishError::MissingAuth)?;
     let password = std::env::var("NEOCITIES_PASSWORD").map_err(|_| PublishError::MissingAuth)?;
 
-    let mut agent = Agent::new();
+    let client = surf::client();
 
-    let response = agent.send(list(&username, &password)).await?;
-    let ListResponse { files, .. } = response.into_body().read_to_json().await?;
+    let mut response = client
+        .send(list(&username, &password))
+        .await
+        .into_anyhow_error()?;
+    let ListResponse { files, .. } = response.body_json().await.into_anyhow_error()?;
     if let Some(existing_page) = files.into_iter().find(|file| file.path == page_name) {
         let digest = Sha1::from(content).digest();
 
@@ -69,14 +69,14 @@ pub async fn publish(page_name: &str, content: &str) -> Result<String, PublishEr
     let data = form_data.end()?;
 
     let auth = base64::encode(format!("{}:{}", username, password));
-    let request = hreq::http::Request::post("https://neocities.org/api/upload")
+    let request = surf::post("https://neocities.org/api/upload")
         .header("authorization", format!("Basic {}", auth))
         .header("content-type", &content_type)
-        .with_body(&data.into_inner())?;
+        .body(data.into_inner());
 
-    let response = agent.send(request).await?;
+    let mut response = client.send(request).await.into_anyhow_error()?;
 
-    let json: serde_json::Value = response.into_body().read_to_json().await?;
+    let json: serde_json::Value = response.body_json().await.into_anyhow_error()?;
     if json["result"].as_str() == Some("error") {
         return Err(PublishError::NeocitiesError(
             json["message"].as_str().unwrap().to_string(),
