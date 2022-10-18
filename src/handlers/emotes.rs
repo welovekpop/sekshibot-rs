31704ad63959a2b2c9e94941930908a884118fcb
Renée Kooi <renee@kooi.me>
2022-10-18 22:18:01 +0200

    emotes: enable js/css minification

--- src/handlers/emotes.rs
+++ src/handlers/emotes.rs
@@ -82,7 +82,11 @@ impl Emotes {
             trs
         );
 
-        let body = minify_html::minify(body.as_bytes(), &minify_html::Cfg::default());
+        let body = minify_html::minify(body.as_bytes(), &minify_html::Cfg {
+            minify_css: true,
+            minify_js: true,
+            ..Default::default()
+        });
 
         let html = html_index::new()
             .raw_body(std::str::from_utf8(&body)?)
