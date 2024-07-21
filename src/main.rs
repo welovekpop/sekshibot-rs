596540ca240256f738f5f92a0c17aaf8f3708e68
Renée <renee@kooi.me>
2024-07-21 10:21:26 +0200

    Add a prometheus metrics endpoint (#2)
    * Add a prometheus metrics endpoint
    
    * --metrics flag

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::process::ExitCode;
 use anyhow::{bail, Result};
 use gumdrop::{Options, ParsingStyle};
+use metrics_exporter_prometheus::PrometheusBuilder;
+use metrics_process::Collector;
 use sekshibot::{ConnectionOptions, SekshiBot, UnauthorizedError};
+use std::{process::ExitCode, time::Duration};
 
-///
+/// The chat moderation-and-more bot for the WLK community.
 #[derive(Debug, Clone, Options)]
 pub struct Cli {
     /// HTTP API endpoint of the üWave server to connect to.
@@ -13,6 +15,63 @@ pub struct Cli {
     #[options(required)]
     pub socket_url: String,
     pub help: bool,
+    pub metrics: Option<String>,
+}
+
+fn setup_metrics(endpoint: &str) -> anyhow::Result<flume::Sender<()>> {
+    let (tx, rx) = flume::bounded::<()>(1);
+
+    let process_metrics = Collector::default();
+    process_metrics.describe();
+
+    let prom_handle = PrometheusBuilder::new()
+        .install_recorder()
+        .expect("failed to install recorder");
+
+    let server = tiny_http::Server::http(endpoint).unwrap();
+
+    let collect_exit = rx.clone();
+    let _collect_handle = std::thread::Builder::new()
+        .name("prometheus collector".into())
+        .spawn(move || loop {
+            match collect_exit.recv_timeout(Duration::from_secs(3)) {
+                Ok(_) => break,
+                Err(flume::RecvTimeoutError::Disconnected) => break,
+                Err(_) => {
+                    process_metrics.collect();
+                }
+            }
+        })
+        .unwrap();
+
+    let server_exit = rx.clone();
+    let _server_handle = std::thread::Builder::new()
+        .name("prometheus exporter".into())
+        .spawn(move || {
+            loop {
+                match server_exit.recv_timeout(Duration::from_millis(50)) {
+                    Ok(_) => break,
+                    Err(flume::RecvTimeoutError::Disconnected) => break,
+                    Err(_) => {}
+                }
+                if let Some(request) = server.recv_timeout(Duration::from_millis(50))? {
+                    if request.url() == "/metrics" || request.url().starts_with("/metrics?") {
+                        let response = tiny_http::Response::from_string(prom_handle.render())
+                            .with_header(
+                                tiny_http::Header::from_bytes(b"content-type", b"text/plain")
+                                    .unwrap(),
+                            );
+                        request.respond(response)?;
+                    } else {
+                        request.respond(tiny_http::Response::empty(tiny_http::StatusCode(404)))?;
+                    }
+                }
+            }
+            anyhow::Ok(())
+        })
+        .unwrap();
+
+    Ok(tx)
 }
 
 fn main() -> Result<ExitCode> {
@@ -29,6 +88,9 @@ fn main() -> Result<ExitCode> {
         _ => bail!("missing SEKSHIBOT_PASSWORD env var"),
     };
 
+    let metrics_endpoint = args.metrics.as_deref().unwrap_or("0.0.0.0:3003");
+    let tx = setup_metrics(&metrics_endpoint)?;
+
     let result = (|| {
         let bot = SekshiBot::connect(ConnectionOptions {
             api_url: args.api_url,
@@ -39,6 +101,9 @@ fn main() -> Result<ExitCode> {
 
         bot.run()
     })();
+
+    let _ = tx.send(());
+    drop(tx);
 
     match result {
         Ok(_) => Ok(ExitCode::SUCCESS),
